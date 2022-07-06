d681391e76a8de10331aea7e832f6081f413c659
grantbergland <grantbergland@gmail.com>
2022-07-06 17:33:38 -0500

    commit tree 8608ca86e3e17e826c9344c7730128beab170bd8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    var cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return cards
+}
+
+function countDown(numbers) {
+while (numbers >= 0){
+    console.log(numbers--);
+    }
+}
+
